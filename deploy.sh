f39e28d0379b0438707eaf1ccfc7de799d6eb2fb
Nimrah12 <nimrahmemon12@gmail.com>
2024-03-11 23:15:23 +0000

    added continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+# Update system packages and install Node.js and npm
+sudo apt update && sudo apt install nodejs npm
+
+# Install pm2 globally
+sudo npm install -g pm2
+
+# Stop the pm2 process named "example_app"
+pm2 stop example_app
+
+# Navigate to the ExampleApplication directory
+cd SimpleApplication/
+
+# Install npm dependencies
+npm install
+
+# Start the pm2 process for your application
+pm2 start ./bin/www --name example_app
